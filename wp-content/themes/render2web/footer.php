da432452a4834df941bc40b987787e96070a154f
JNCamil <juancamvh@hotmail.com>
2024-09-11 20:24:45 +0200

    configuraciones iniciales

---
+++ wp-content/themes/render2web/footer.php
@@ -0,0 +1,18 @@
+<div class="col-md-4 modulo-sidebar">
+             <h4>Calendario</h4>
+             <img src="img/Calendario.jpg" class="img-responsive">
+          </div>
+
+
+      </div><!-- /.row -->
+
+    </div><!-- /.container -->
+
+     <footer class="blog-footer">
+      <p>© <?php date('Y'); ?> - <?php bloginfo('name'); ?></p>
+      <p> <?php bloginfo('description'); ?> </p>
+    </footer>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  </body>
+</html>

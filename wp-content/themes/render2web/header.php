35cfdc1f7c426759e3c675ce34a4fc86077cded6
JNCamil <juancamvh@hotmail.com>
2024-09-28 17:41:12 +0200

    configuracion del menú

--- wp-content/themes/render2web/header.php
+++ wp-content/themes/render2web/header.php
@@ -22,13 +22,28 @@
   <body>
     <div class="blog-masthead">
       <div class="container">
-        <nav class="blog-nav">
-          <a class="blog-nav-item active" href="#">Inicio</a>
-          <a class="blog-nav-item" href="#">Página 1</a>
-          <a class="blog-nav-item" href="#">Página 2</a>
-          <a class="blog-nav-item" href="#">Página 3</a>
-          <a class="blog-nav-item" href="#">Contacto</a>
-        </nav>
+      <nav class="navbar navbar-expand-md navbar-dark ">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-menu" aria-controls="main-menu" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        
+        <div class="collapse navbar-collapse" id="main-menu">
+            <?php
+            wp_nav_menu(array(
+                'theme_location' => 'main-menu',
+                'container' => false,
+                'menu_class' => '',
+                'fallback_cb' => '__return_false',
+                'items_wrap' => '<ul id="%1$s" class="navbar-nav me-auto mb-2 mb-md-0 %2$s">%3$s</ul>',
+                'depth' => 2,
+                'walker' => new bootstrap_5_wp_nav_menu_walker()
+            ));
+            ?>
+        </div>
+    </div>
+</nav>
       </div>
     </div>
 
